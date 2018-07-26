3e2f8457829fc2fd903b035929c4dde09201099e
icolbert <icolbert@local>
2018-07-26 03:02:12 +0000

    Updated models

--- keras-sign/classifier/processor.py
+++ keras-sign/classifier/processor.py
@@ -2,13 +2,11 @@ import os, sys
 
 def save_keras(model_name, kmodel, ep):
     modelJSON = kmodel.to_json()
-    with open(os.path.join("classifier","{0}-{1}.json".format(model_name, ep), "w") as jfile:
+    with open(os.path.join("classifier","{0}-{1}.json".format(model_name, ep), "w")) as jfile:
         jfile.write(modelJSON)
 
     kmodel.save_weights(os.path.join("classifier", "{0}-{1}.h5".format(model_name, ep)))
-    if self.verbose: print("Saved model to %s" % os.path.join("classifier", "{0}-{1}".format(model_name, ep)))
-    else:
-        raise Exception("Set model path.")
+    print("Saved model to %s" % os.path.join("classifier", "{0}-{1}".format(model_name, ep)))
 
 def load_keras(ep):
     jfile = open(os.path.join(model_path, "{0}-{1}.json".format(model_name, ep)))
@@ -16,7 +14,5 @@ def load_keras(ep):
             
     kmodel = model_from_json(modelJSON)
     kmodel.load_weights(os.path.join(model_path, "{0}-{1}.h5".format(model_name, ep)))
-    if self.verbose: print("Loaded Keras model from {0}!".format(os.path.join(model_path)))
-        return kmodel
-    else:
-        raise Exception("Set model path.")
+    print("Loaded Keras model from {0}!".format(os.path.join(model_path)))
+    return kmodel
